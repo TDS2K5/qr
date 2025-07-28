6089ce9a6782d4366a5e0044663a8a4ba8709ee8
TDS2K5 <126385545+TDS2K5@users.noreply.github.com>
2025-07-29 01:16:40 +0530

    streamlit

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+import streamlit as st
+from pyzbar.pyzbar import decode
+from PIL import Image
 
-app = Flask(__name__)
+st.title("QR Code Decoder")
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+uploaded_file = st.file_uploader("Upload a QR ", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    result = decode(image)
+    if result:
+        
+        st.success(f"Decoded text: {result[0].data.decode('utf-8')}")
+    else:
+        st.warning("No QR code found.")
