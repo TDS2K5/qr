ff739fb1419b57aa0487a0be417f65d78a03c50d
TDS2K5 <126385545+TDS2K5@users.noreply.github.com>
2025-08-17 21:30:20 +0530

    added pages

--- home.py
+++ home.py
@@ -5,7 +5,7 @@ st.markdown(
         """
         Detect - Upload an image containing a QR code to instantly convert it into a text/link. \n
         \n
-        Gen - Convert text/link into a custom QR code of your own! 
+        Gen - Generate custom QR code of your own by providing text/link input! 
     """
     )
 
